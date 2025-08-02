dc161c06539e287a0125d40bb8abf0a25eebb2fe
w0nd3rl4nd <alicealiagamoreno@gmail.com>
2025-08-02 15:07:12 +0200

    Configure studioHost

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,13 +1,10 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'qjwzm36f',
     dataset: 'production'
   },
-  /**
-   * Enable auto-updates for studios.
-   * Learn more at https://www.sanity.io/docs/cli#auto-updates
-   */
+  studioHost: 'mrpass',
   autoUpdates: true,
 })
